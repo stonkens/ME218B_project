2e528cb48e394b54e9b6c36b3ac1a371b4f2598e
Kristine Y. Chen <kchen7@stanford.edu>
2019-02-24 23:05:39 -0800

    Does another team's code work on our system???

--- FrameworkCode/Source/DCMotorService.c
+++ FrameworkCode/Source/DCMotorService.c
@@ -54,6 +54,10 @@ static void setTransportDuty(uint32_t duty);
 static void RestorePickupDC(void); 
 static void RestoreTransportDC(void); 
 
+static uint32_t Startup_Period_ms = 5;
+static uint32_t Period_0; // ticks
+static uint32_t Duty_0;   // percent
+
 /*---------------------------- Module Variables ---------------------------*/
 static uint8_t    MyPriority;
 /*------------------------------ Module Code ------------------------------*/
@@ -222,11 +226,82 @@ static void RestoreTransportDC(void)
   HWREG(PWM0_BASE + PWM_O_0_GENA) = GenA_Normal;
 }
 
+void PabloTeamsCode(void){
+  {
+      // start by enabling the clock to the PWM Module (PWM0)
+      HWREG(SYSCTL_RCGCPWM) |= SYSCTL_RCGCPWM_R0;
+
+      // enable the clock to Port B
+      HWREG(SYSCTL_RCGCGPIO) |= SYSCTL_RCGCGPIO_R1;
+
+      // make sure that Port B is initialized
+      while ((HWREG(SYSCTL_PRGPIO) & SYSCTL_PRGPIO_R1) != BIT1HI)
+      {}
+
+      // Select the PWM clock as System Clock/32
+      HWREG(SYSCTL_RCC) = (HWREG(SYSCTL_RCC) & ~SYSCTL_RCC_PWMDIV_M) |
+          (SYSCTL_RCC_USEPWMDIV | SYSCTL_RCC_PWMDIV_32);
+
+      // make sure that the PWM module clock has gotten going
+      while ((HWREG(SYSCTL_PRPWM) & SYSCTL_PRPWM_R0) != SYSCTL_PRPWM_R0)
+      {}
+
+      // disable the PWM while initializing
+      HWREG(PWM0_BASE + PWM_O_0_CTL) = 0;
+
+      // program generators to go to 1 at rising compare A/B, 0 on falling
+      // compare A/B
+      HWREG(PWM0_BASE + PWM_O_0_GENA) = GenA_Normal;
+      HWREG(PWM0_BASE + PWM_O_0_GENB) = GenB_Normal;
+
+      // Set the PWM period. Since we are counting both up & down, we
+      // initialize the load register to 1/2 the desired total period. We
+      // will also program the match compare registers to 1/2 the desired
+      // high time
+      Period_0 = Startup_Period_ms * PWMTicksPerMS;
+      HWREG(PWM0_BASE + PWM_O_0_LOAD) = ((Period_0)) >> 1;
+
+      // Set the initial Duty cycle on A to 50% by programming the compare
+      // value to 1/2 the period to count up (or down). Technically, the
+      // value to program should be Period/2 - DesiredHighTime/2, but since
+      // the desired high time is 1/2 the period, we can skip the subtract
+      Duty_0 = 50;
+      HWREG(PWM0_BASE + PWM_O_0_CMPA) = HWREG(PWM0_BASE + PWM_O_0_LOAD) >> 1;
+      // Set the initial Duty cycle on B to 50% like A
+      HWREG(PWM0_BASE + PWM_O_0_CMPB) = HWREG(PWM0_BASE + PWM_O_0_LOAD) >> 1;
+
+      // enable the PWM outputs
+      HWREG(PWM0_BASE + PWM_O_ENABLE) |= (PWM_ENABLE_PWM0EN | PWM_ENABLE_PWM1EN);
+
+      // now configure the Port B pins to be PWM outputs
+      // start by selecting the alternate function for PB6 and PB7
+      HWREG(GPIO_PORTB_BASE + GPIO_O_AFSEL) |= (BIT6HI | BIT7HI);
+
+      // now choose to map PWM to those pins, this is a mux value of 4 that
+      // we want to use for specifying the function on bits 6 and 7
+      HWREG(GPIO_PORTB_BASE + GPIO_O_PCTL) =
+          (HWREG(GPIO_PORTB_BASE + GPIO_O_PCTL) & 0x00ffffff)
+          + (4 << (6 * BitsPerNibble)) + (4 << (7 * BitsPerNibble));
+
+      // Enable pins 6 and 7 on Port B for digital I/O
+      HWREG(GPIO_PORTB_BASE + GPIO_O_DEN) |= (BIT6HI | BIT7HI);
+      // make pins 6 and 7 on Port B an output
+      HWREG(GPIO_PORTB_BASE + GPIO_O_DIR) |= (BIT6HI | BIT7HI);
+
+      // set the up/down count mode, enable the PWM generator and make
+      // both generator updates locally synchronized to zero count
+      HWREG(PWM0_BASE + PWM_O_0_CTL) = (PWM_0_CTL_MODE | PWM_0_CTL_ENABLE |
+          PWM_0_CTL_GENAUPD_LS | PWM_0_CTL_GENBUPD_LS);
+    }
+}
+
 void InitPWMDemo( void ){
 // start by enabling the clock to the PWM Module (PWM0)
 HWREG(SYSCTL_RCGCPWM) |= SYSCTL_RCGCPWM_R0;
 // enable the clock to Port B
 HWREG(SYSCTL_RCGCGPIO) |= SYSCTL_RCGCGPIO_R1;
+       while ((HWREG(SYSCTL_PRGPIO) & SYSCTL_PRGPIO_R1) != BIT1HI)
+      {}; 
 // Select the PWM clock as System Clock/32
 HWREG(SYSCTL_RCC) = (HWREG(SYSCTL_RCC) & ~SYSCTL_RCC_PWMDIV_M) |
 (SYSCTL_RCC_USEPWMDIV | SYSCTL_RCC_PWMDIV_32);
@@ -249,6 +324,7 @@ HWREG( PWM0_BASE+PWM_O_0_LOAD) = ((PeriodInMS * PWMTicksPerMS))>>1;
 // should be Period/2 - DesiredHighTime/2, but since the desired high time is 1/2
 // the period, we can skip the subtract
 HWREG( PWM0_BASE+PWM_O_0_CMPA) = HWREG( PWM0_BASE+PWM_O_0_LOAD)>>1;
+//HWREG( PWM0_BASE+PWM_O_0_CMPB) = HWREG( PWM0_BASE+PWM_O_0_LOAD)>>1;
 // Set the initial Duty cycle on B to 25% by programming the compare value
 // to Period/2 - Period/8 (75% of the period)
 HWREG( PWM0_BASE+PWM_O_0_CMPB) = (HWREG( PWM0_BASE+PWM_O_0_LOAD)) -
@@ -373,8 +449,9 @@ int main(void)
   PortFunctionInit();
 
   // Your hardware initialization function calls go here
-  InitPWMDemo(); //this outputs two waveforms of 25% duty cycle (CMPB) CMPA NEVER works
-  InitDCPWM();
+  PabloTeamsCode(); 
+  //InitPWMDemo(); //this outputs two waveforms of 25% duty cycle (CMPB) CMPA NEVER works
+  //InitDCPWM();
   //startPickupMotor(25); 
   //printf("\r\n Started pickup motor");
   //stopPickupMotor(); 
