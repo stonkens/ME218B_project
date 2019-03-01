f3ddf095c282a2f2478f760ec0f4ab753b0e8864
stonkens <stonkens@stanford.edu>
2019-03-01 15:20:34 -0800

    CollectingSM basic functionality mapped

--- FrameworkCode/Headers/CollectingSM.h
+++ FrameworkCode/Headers/CollectingSM.h
@@ -4,23 +4,23 @@
  3/17/09  Fixed prototpyes to use Event_t
  ****************************************************************************/
 
-#ifndef GamePlayHSM_H
-#define GamePlayHSM_H
+#ifndef CollectingSM_H
+#define CollectingSM_H
 
 #include <stdbool.h>
 #include "ES_Events.h"
-#define BALL_COLLECTION_TIME 20000
+
 
 // typedefs for the states
 // State definitions for use with the query function
-typedef enum { COLLECTING_GARBAGE, RECYCLING, LANDFILLING } GamePlayState_t ;
+typedef enum { ORIENTING, ROAMING} CollectingState_t ;
 
 
 // Public Function Prototypes
 
-ES_Event_t RunGamePlaySM( ES_Event_t CurrentEvent );
-void StartGamePlaySM ( ES_Event_t CurrentEvent );
-GamePlayState_t QueryGamePlaySM ( void );
+ES_Event_t RunCollectingSM( ES_Event_t CurrentEvent );
+void StartCollectingSM ( ES_Event_t CurrentEvent );
+CollectingState_t QueryCollectingSM ( void );
 
-#endif /*GamePlayHSM_H */
+#endif /*CollectingSM_H */
 
--- FrameworkCode/Source/CollectingSM.c
+++ FrameworkCode/Source/CollectingSM.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  Module
-   GamePlaySM.c
+   CollectingSM.c
 
  Revision
    2.0.1
@@ -62,25 +62,25 @@
 // define constants for the states for this machine
 // and any other local defines
 
-#define ENTRY_STATE COLLECTING_GARBAGE
+#define ENTRY_STATE ORIENTING
 
 /*---------------------------- Module Functions ---------------------------*/
 /* prototypes for private functions for this machine, things like during
    functions, entry & exit functions.They should be functions relevant to the
    behavior of this state machine
 */
-static ES_Event_t DuringCollectingGarbage( ES_Event_t Event);
-static ES_Event_t DuringRecycling(ES_Event_t Event);
+static ES_Event_t DuringOrienting( ES_Event_t Event);
+static ES_Event_t DuringRoaming(ES_Event_t Event);
 static ES_Event_t DuringLandfilling( ES_Event_t Event);
 
 /*---------------------------- Module Variables ---------------------------*/
 // everybody needs a state variable, you may need others as well
-static GamePlayState_t CurrentState;
+static CollectingState_t CurrentState;
 
 /*------------------------------ Module Code ------------------------------*/
 /****************************************************************************
  Function
-    RunGamePlaySM
+    RunCollectingSM
 
  Parameters
    ES_Event_t: the event to process
@@ -95,62 +95,24 @@ static GamePlayState_t CurrentState;
  Author
    J. Edward Carryer, 2/11/05, 10:45AM
 ****************************************************************************/
-ES_Event_t RunGamePlaySM( ES_Event_t CurrentEvent )
+ES_Event_t RunCollectingSM( ES_Event_t CurrentEvent )
 {
    bool MakeTransition = false;/* are we making a state transition? */
-   GamePlayState_t NextState = CurrentState;
+   CollectingState_t NextState = CurrentState;
    ES_Event_t EntryEventKind = { ES_ENTRY, 0 };// default to normal entry to new state
    ES_Event_t ReturnEvent = CurrentEvent; // assume we are not consuming event
 
    switch ( CurrentState )
    {
-       case COLLECTING_GARBAGE :      
+       case ORIENTING :      
 			 {
-         ReturnEvent = CurrentEvent = DuringCollectingGarbage(CurrentEvent);
+         ReturnEvent = CurrentEvent = DuringOrienting(CurrentEvent);
          //process any events
          if ( CurrentEvent.EventType != ES_NO_EVENT ) //If an event is active
          {
             switch (CurrentEvent.EventType)
             {
 							 
-             case ES_TIMEOUT :
-             {
-               if (CurrentEvent.EventParam == BALL_COLLECTION_TIMER)
-               {
-                  if (QueryRecycleBalls() != 0)
-                  {
-                    // Execute action function for state one : event one
-                    NextState = RECYCLING;//Decide what the next state will be
-                    // for internal transitions, skip changing MakeTransition
-                    MakeTransition = true; //mark that we are taking a transition
-                    // if transitioning to a state with history change kind of entry
-                    EntryEventKind.EventType = ES_ENTRY;
-                    // optionally, consume or re-map this event for the upper
-                    // level state machine
-                    ReturnEvent.EventType = ES_NO_EVENT;
-                  }
-                  
-                  else if(QueryLandFillBalls() != 0)
-                  {
-                    // Execute action function for state one : event one
-                    NextState = LANDFILLING;//Decide what the next state will be
-                    // for internal transitions, skip changing MakeTransition
-                    MakeTransition = true; //mark that we are taking a transition
-                    // if transitioning to a state with history change kind of entry
-                    EntryEventKind.EventType = ES_ENTRY;
-                    // optionally, consume or re-map this event for the upper
-                    // level state machine
-                    ReturnEvent.EventType = ES_NO_EVENT;
-                  }
-                  else
-                  {
-                    //This time we only allow for BALL_COLLECTION_TIME/2 seconds to query again
-                    ES_Timer_InitTimer(BALL_COLLECTION_TIMER, BALL_COLLECTION_TIME/2);
-                  }
-                 
-								 }
-							 }
-							 break;
 							 
 							 default:
 							 {;
@@ -164,46 +126,15 @@ ES_Event_t RunGamePlaySM( ES_Event_t CurrentEvent )
     }
 		break;
 		
-		case RECYCLING:
+		case ROAMING:
 		{
-         ReturnEvent = CurrentEvent = DuringRecycling(CurrentEvent);
+         ReturnEvent = CurrentEvent = DuringRoaming(CurrentEvent);
          //process any events
          if ( CurrentEvent.EventType != ES_NO_EVENT ) //If an event is active
          {
             switch (CurrentEvent.EventType)
             {
                
-               case EV_RECYCLING_DONE :
-               {
-                  if(QueryLandFillBalls() != 0)
-                  {
-                    NextState = LANDFILLING;
-                    // for internal transitions, skip changing MakeTransition
-                    MakeTransition = true; //mark that we are taking a transition
-                    // if transitioning to a state with history change kind of entry
-                    EntryEventKind.EventType = ES_ENTRY;
-                    // optionally, consume or re-map this event for the upper
-                    // level state machine
-                    ReturnEvent.EventType = ES_NO_EVENT;
-                  }
-                  
-                  else
-                  {
-
-                    NextState = COLLECTING_GARBAGE;
-                    // for internal transitions, skip changing MakeTransition
-                    MakeTransition = true; //mark that we are taking a transition
-                    // if transitioning to a state with history change kind of entry
-                    EntryEventKind.EventType = ES_ENTRY;
-                    // optionally, consume or re-map this event for the upper
-                    // level state machine
-                    ReturnEvent.EventType = ES_NO_EVENT;	
-                    //Restart Timer to check for balls
-                    ES_Timer_InitTimer(BALL_COLLECTION_TIMER, BALL_COLLECTION_TIME);
-                  }
-               }
-               break;
-                
 							 default:
                {;
 							 }
@@ -216,60 +147,6 @@ ES_Event_t RunGamePlaySM( ES_Event_t CurrentEvent )
 		}
 		break;
 		
-		case LANDFILLING:
-		{
-         ReturnEvent = CurrentEvent = DuringRecycling(CurrentEvent);
-         //process any events
-         if ( CurrentEvent.EventType != ES_NO_EVENT ) //If an event is active
-         {
-            switch (CurrentEvent.EventType)
-            {
-							 
-               case EV_LANDFILLING_DONE:
-               {
-                  if(QueryRecycleBalls() != 0)
-                  {
-                    NextState = RECYCLING;
-                    // for internal transitions, skip changing MakeTransition
-                    MakeTransition = true; //mark that we are taking a transition
-                    // if transitioning to a state with history change kind of entry
-                    EntryEventKind.EventType = ES_ENTRY;
-                    // optionally, consume or re-map this event for the upper
-                    // level state machine
-                    ReturnEvent.EventType = ES_NO_EVENT;
-                    
-                  }
-                  
-                  else
-                  {
-
-                    
-                    NextState = COLLECTING_GARBAGE;
-                    // for internal transitions, skip changing MakeTransition
-                    MakeTransition = true; //mark that we are taking a transition
-                    // if transitioning to a state with history change kind of entry
-                    EntryEventKind.EventType = ES_ENTRY;
-                    // optionally, consume or re-map this event for the upper
-                    // level state machine
-                    ReturnEvent.EventType = ES_NO_EVENT;	
-                    //Restart Timer to check for balls
-                    ES_Timer_InitTimer(BALL_COLLECTION_TIMER, BALL_COLLECTION_TIME);
-                  }
-               }
-               break;
-							 
-							 
-							 default:
-							{;
-							}
-                // repeat cases as required for relevant events
-            }
-
-         }
-       
-      // repeat state pattern as required for other states
-		}
-		break;
 		default:
 		{;
 		}
@@ -279,19 +156,19 @@ ES_Event_t RunGamePlaySM( ES_Event_t CurrentEvent )
     {
        //   Execute exit function for current state
        CurrentEvent.EventType = ES_EXIT;
-       RunGamePlaySM(CurrentEvent);
+       RunCollectingSM(CurrentEvent);
 
        CurrentState = NextState; //Modify state variable
 
        //   Execute entry function for new state
        // this defaults to ES_ENTRY
-       RunGamePlaySM(EntryEventKind);
+       RunCollectingSM(EntryEventKind);
      }
      return(ReturnEvent);
 }
 /****************************************************************************
  Function
-     StartGamePlaySM
+     StartCollectingSM
 
  Parameters
      None
@@ -306,7 +183,7 @@ ES_Event_t RunGamePlaySM( ES_Event_t CurrentEvent )
  Author
      J. Edward Carryer, 2/18/99, 10:38AM
 ****************************************************************************/
-void StartGamePlaySM ( ES_Event_t CurrentEvent )
+void StartCollectingSM ( ES_Event_t CurrentEvent )
 {
    // to implement entry to a history state or directly to a substate
    // you can modify the initialization of the CurrentState variable
@@ -317,7 +194,7 @@ void StartGamePlaySM ( ES_Event_t CurrentEvent )
         CurrentState = ENTRY_STATE;
    }
    // call the entry function (if any) for the ENTRY_STATE
-   RunGamePlaySM(CurrentEvent);
+   RunCollectingSM(CurrentEvent);
 }
 
 /****************************************************************************
@@ -328,7 +205,7 @@ void StartGamePlaySM ( ES_Event_t CurrentEvent )
      None
 
  Returns
-     GamePlayState_t The current state of the Template state machine
+     CollectingState_t The current state of the Template state machine
 
  Description
      returns the current state of the Template state machine
@@ -337,7 +214,7 @@ void StartGamePlaySM ( ES_Event_t CurrentEvent )
  Author
      J. Edward Carryer, 2/11/05, 10:38AM
 ****************************************************************************/
-GamePlayState_t QueryGamePlaySM(void)
+CollectingState_t QueryCollectingSM(void)
 {
    return CurrentState;
 }
@@ -346,7 +223,7 @@ GamePlayState_t QueryGamePlaySM(void)
  private functions
  ***************************************************************************/
 
-static ES_Event_t DuringCollectingGarbage( ES_Event_t Event)
+static ES_Event_t DuringOrienting( ES_Event_t Event)
 {
     ES_Event_t ReturnEvent = Event; // assume no re-mapping or consumption
 
@@ -393,49 +270,9 @@ static ES_Event_t DuringCollectingGarbage( ES_Event_t Event)
 }
 
 
-static ES_Event_t DuringRecycling( ES_Event_t Event)
+static ES_Event_t DuringRoaming( ES_Event_t Event)
 {
     ES_Event_t ReturnEvent = Event; // assume no re-mapping or consumption
-
-    // process ES_ENTRY, ES_ENTRY_HISTORY & ES_EXIT events
-    if ( (Event.EventType == ES_ENTRY) ||
-         (Event.EventType == ES_ENTRY_HISTORY) )
-    {
-        // implement any entry actions required for this state machine
-        
-			
-				
-        // after that start any lower level machines that run in this state
-        //StartLowerLevelSM( Event );
-        // repeat the StartxxxSM() functions for concurrent state machines
-        // on the lower level
-    }
-    else if ( Event.EventType == ES_EXIT )
-    {
-        // on exit, give the lower levels a chance to clean up first
-        //RunLowerLevelSM(Event);
-        // repeat for any concurrently running state machines
-        // now do any local exit functionality
-			
-      
-    }else
-    // do the 'during' function for this state
-    {
-        // run any lower level state machine
-        // ReturnEvent = RunLowerLevelSM(Event);
-      
-        // repeat for any concurrent lower level machines
-      
-        // do any activity that is repeated as long as we are in this state
-    }
-    // return either Event, if you don't want to allow the lower level machine
-    // to remap the current event, or ReturnEvent if you do want to allow it.
-    return(ReturnEvent);
-}
-
-static ES_Event_t DuringLandfilling( ES_Event_t Event)
-{
-   ES_Event_t ReturnEvent = Event; // assume no re-mapping or consumption
 
     // process ES_ENTRY, ES_ENTRY_HISTORY & ES_EXIT events
     if ( (Event.EventType == ES_ENTRY) ||
